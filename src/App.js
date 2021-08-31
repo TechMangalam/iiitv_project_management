ef1db1b4f95718d9f8f86b31a8c2b6cefc34a349
TechMangalam <201951089@iiitvadodara.ac.in>
2021-08-31 12:06:01 +0530

    ghnf

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>IIITV Project Management</h1>
+      <h3>Software Engineering Project</h3>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
